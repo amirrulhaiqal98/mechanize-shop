70213a0c706f387083009c717cdc172ecd294b2c
amirrulhaiqal98 <63889086+amirrulhaiqal98@users.noreply.github.com>
2024-05-13 11:13:00 +0800

    make Admin Middleware in laravel | redirect admin login to dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
@@ -25,7 +28,16 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    // protected $redirectTo = '/home';
+
+    protected function authenticated()
+    {
+        if(Auth::user()->role_as == '1'){
+            return redirect('admin/dashboard')->with('message','Welcome to Dasboard');
+        }else{
+            return redirect('/home')->with('message','Logged in Successfully');
+        }
+    }
 
     /**
      * Create a new controller instance.
--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
@@ -15,6 +16,9 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if(!Auth::user()->role_as == '1'){
+            return redirect('/home')->with('status','Access Denied, As you are not Admin');
+        }
         return $next($request);
     }
 }
