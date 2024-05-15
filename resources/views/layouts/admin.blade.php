2a02ec8f4cbd6c760bcdfca95c97ed4cb2309358
amirrulhaiqal98 <63889086+amirrulhaiqal98@users.noreply.github.com>
2024-05-15 09:37:49 +0800

    Add Category in ecommerce
    creating migration, model, controller

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,59 +9,56 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-	<!-- plugins:css -->
-	<link rel="stylesheet" href="{{asset('admin/vendors/mdi/css/materialdesignicons.min.css')}}">
-	<link rel="stylesheet" href="{{asset('admin/vendors/css/vendor.bundle.base.css')}}">
-	<!-- endinject -->
-	<!-- plugin css for this page -->
-	{{-- <link rel="stylesheet" href="{{asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}"> --}}
-	<!-- End plugin css for this page -->
-	<!-- inject:css -->
-	<link rel="stylesheet" href="{{asset('admin/css/style.css')}}">
-	<!-- endinject -->
-	<link rel="shortcut icon" href="{{asset('admin/images/favicon.ico')}}" />
-    
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('admin/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/vendors/css/vendor.bundle.base.css') }}">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <link rel="stylesheet" href="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('admin/css/style.css') }}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{ asset('admin/images/favicon.ico') }}" />
+
     @livewireStyles
 </head>
 <body>
-	<div class="container-scroller">
-		@include('layouts.inc.admin.navbar')
-		<!-- partial -->
-		<div class="container-fluid page-body-wrapper"> 
-			@include('layouts.inc.admin.sidebar')
+    <div class="container-scroller">
+        @include('layouts.inc.admin.navbar')
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper"> 
+            @include('layouts.inc.admin.sidebar')
 
-			      <!-- partial -->
-				  <div class="main-panel">
-					<div class="content-wrapper">
-						@yield('content')
-					</div>
-				  </div>
-			
-		</div>
-	</div>
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
 
-
-@livewireScripts
-	<!-- plugins:js -->
-	<script src="vendors/js/vendor.bundle.base.js"></script>
-	<!-- endinject -->
-	<!-- Plugin js for this page-->
-	<script src="vendors/chart.js/chart.umd.js"></script>
-	<script src="vendors/datatables.net/jquery.dataTables.js"></script>
-	<script src="vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
-	<!-- End plugin js for this page-->
-	<!-- inject:js -->
-	<script src="js/off-canvas.js"></script>
-	<script src="js/hoverable-collapse.js"></script>
-	<script src="js/template.js"></script>
-	<script src="js/settings.js"></script>
-	<script src="js/todolist.js"></script>
-	<!-- endinject -->
-	<!-- Custom js for this page-->
-	<script src="js/dashboard.js"></script>
-		<script src="js/proBanner.js"></script>
-
-	<!-- End custom js for this page-->
-	<script src="js/jquery.cookie.js" type="text/javascript"></script>
-	</body>
+    @livewireScripts
+    <!-- plugins:js -->
+    <script src="{{ asset('admin/vendors/js/vendor.bundle.base.js') }}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <script src="{{ asset('admin/vendors/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="{{ asset('admin/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('admin/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('admin/js/template.js') }}"></script>
+    <script src="{{ asset('admin/js/settings.js') }}"></script>
+    <script src="{{ asset('admin/js/todolist.js') }}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="{{ asset('admin/js/dashboard.js') }}"></script>
+    <script src="{{ asset('admin/js/proBanner.js') }}"></script>
+    <!-- End custom js for this page-->
+    <script src="{{ asset('admin/js/jquery.cookie.js') }}" type="text/javascript"></script>
+</body>
 </html>
